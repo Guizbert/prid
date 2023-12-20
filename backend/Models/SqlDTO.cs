b0723bda455110a2f02eaf21c3e0ff5ba1b66062
alberich ravier <albe.ravier@hotmail.fr>
2023-12-20 14:44:31 +0100

    build (backEnd) creation model sqlDTO et sqlsolutionDTO pour l'envoie de la query du user et check si c'est correct

---
+++ backend/Models/SqlDTO.cs
@@ -0,0 +1,92 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using prid_2324_a12.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using prid_2324_a12.Helpers;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace prid_2324_a12.Models;
+
+public class SqlDTO{
+
+    public int QuestionId{get;set;}
+    public string Query{get;set;} = null!;
+    public string DbName{get; set;}=null!;
+
+
+    public SqlSolutionDTO ExecuteQuery()
+    {
+        // Your connection string, replace with actual details
+        string connectionString = $"server=localhost;database={this.DbName};uid=root;password=root";
+
+        using MySqlConnection connection = new MySqlConnection(connectionString);
+        DataTable table = new DataTable();
+
+        try
+        {
+            connection.Open();
+            MySqlCommand command = new MySqlCommand($"SET sql_mode = 'STRICT_ALL_TABLES'; {this.Query}", connection);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+            adapter.Fill(table);
+        }
+        catch (Exception e)
+        {
+            // Handle the exception
+            Console.WriteLine($"Error: {e.Message}");
+            return new SqlSolutionDTO
+            {
+                Error = new string[] { e.Message }
+            }; // Return SqlSolutionDTO with the error message
+        }
+
+        // Get column names
+        string[] columns = new string[table.Columns.Count];
+        for (int i = 0; i < table.Columns.Count; ++i)
+            columns[i] = table.Columns[i].ColumnName;
+
+        Console.WriteLine("Columns: ");
+        Console.WriteLine(string.Join(", ", columns));
+
+        // Get data
+        string[][] data = new string[table.Rows.Count][];
+        for (int j = 0; j < table.Rows.Count; ++j)
+        {
+            data[j] = new string[table.Columns.Count];
+            for (int i = 0; i < table.Columns.Count; ++i)
+            {
+                object value = table.Rows[j][i];
+                string str;
+                if (value == null)
+                    str = "NULL";
+                else
+                {
+                    if (value is DateTime d)
+                    {
+                        if (d.TimeOfDay == TimeSpan.Zero)
+                            str = d.ToString("yyyy-MM-dd");
+                        else
+                            str = d.ToString("yyyy-MM-dd hh:mm:ss");
+                    }
+                    else
+                        str = value?.ToString() ?? "";
+                }
+                data[j][i] = str;
+            }
+        }
+
+        return new SqlSolutionDTO(){
+            Data = data, ColumnNames = columns, DbName = this.DbName
+        };
+    }
+
+    // public bool Compare(SqlDTO d2){
+
+    // }
+    
+}
---
+++ backend/Models/SqlSolutionDTO.cs
@@ -0,0 +1,38 @@
+namespace prid_2324_a12.Models;
+
+public class SqlSolutionDTO{
+    public string[] Error{get;set;} = new string[0];
+    public string CorrectMessage{get;set;} = "";
+    public string[] ColumnNames{get;set;} = new string[0];
+    public string[][] Data{get;set;} = new string[0][];
+    public string DbName{get; set;}="";
+    public string[] solutions{get;set;} =new string[0];
+    
+
+
+    public SqlSolutionDTO CheckQueries(SqlSolutionDTO solutionQuery){
+        //check col length et data lenght
+        //Console.WriteLine("data : " + this.Data[1].Length +"\n solution data : "+solutionQuery.Data[1].Length);
+        if(this.ColumnNames.Length != solutionQuery.ColumnNames.Length){
+            this.Error = new string[]{ "Incorrect length for Columns"};
+        }
+        if(this.Data.Length != solutionQuery.Data.Length){
+            this.Error = new string[]{ "Incorrect length for Rows"};
+        }
+             // mettre data et col dans un array a une dimension et check CHAQUE ELEMENTS DE CHAQUES ARRAY
+        string[] userArray = this.ColumnNames.Concat(this.Data.SelectMany(row => row)).ToArray();
+        string[] soluceArray = solutionQuery.ColumnNames.Concat(solutionQuery.Data.SelectMany(row => row)).ToArray();
+        for(int i =0; i < userArray.Length; ++i){
+            Console.WriteLine("data object ---> : " + userArray[i] +"\n solution data  ---> : "+soluceArray[i]);
+            if(userArray[i] != soluceArray[i]){
+                this.Error = new string[]{ "Incorrect data"};break; //s'arrête dès qu'on a une erreur
+            }
+        }
+        if (this.Error.Length ==0){
+            this.CorrectMessage = "Votre requête a retourné une réponse correcte ! \nNéanmoins, comparez votre solution avec celle(s) ci-dessous pour voir si vous n'avez pas eu un peu de chance :)";
+        }
+        
+        //return this car c'est ce qu'on veut return dans la page.
+        return this; 
+    }
+}   
