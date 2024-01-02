5649a4860b8e956aa22b6cb7943459b00a377950
alberich ravier <albe.ravier@hotmail.fr>
2024-01-02 17:24:09 +0100

    BUILD (back) modification question controller pour la fonction sql pour faire des appels aux models

--- backend/Controllers/QuestionController.cs
+++ backend/Controllers/QuestionController.cs
@@ -85,7 +85,7 @@ public class QuestionController : ControllerBase
 
     [AllowAnonymous]
     [HttpGet("getFirstQuestionReadOnly/{quizId}/{userId}")]
-    public async Task<Question?> getFirstQuestionReadOnly(int quizId, int userId){
+    public async Task<Question?> GetFirstQuestionReadOnly(int quizId, int userId){
         var question = await _context.Questions.Where(q => q.QuizId == quizId).ToListAsync();
         return question[0];
     }
@@ -133,11 +133,8 @@ public class QuestionController : ControllerBase
     public async Task<IActionResult> PostQuestion(QuestionDTO question){
         // Mapper le QuestionDTO vers Question
         var newQuestion = _mapper.Map<Question>(question);
-
         _context.Questions.Add(newQuestion);
-
         await _context.SaveChangesAsync();
-
         return CreatedAtAction(nameof(GetOne), new { id = newQuestion.Id }, _mapper.Map<QuestionDTO>(newQuestion));
     }
 
@@ -160,12 +157,9 @@ public class QuestionController : ControllerBase
     [Authorized(Role.Teacher, Role.Student, Role.Admin)]
     [HttpGet("getdata/{dbname}")]
     public async Task<ActionResult<Dictionary<string, List<string>>>> getdata(string dbname){
-        
         string connectionString = $"server=localhost;database={dbname};uid=root;password=root";
-
         using MySqlConnection connection = new MySqlConnection(connectionString);
         List<string> tableNames = new List<string>();
-
         try
         {
             connection.Open();
@@ -219,10 +213,7 @@ public class QuestionController : ControllerBase
     [HttpPost("querySent")]
     public async Task<ActionResult<object>> Sql(SqlDTO sqldto)
     {
-
         string soluce = await _context.Solutions.Where(s => s.QuestionId == sqldto.QuestionId).Select(s => s.Sql).FirstOrDefaultAsync();
-        //var soluce = await getFirstSoluce(sqldto.QuestionId);
-
         if (soluce == null)
         {
             // Handle the case where no solution is found
@@ -233,44 +224,45 @@ public class QuestionController : ControllerBase
             Query = soluce!,
             DbName = sqldto.DbName
         };
-        
         SqlSolutionDTO userQuery = sqldto.ExecuteQuery();
-        
-        if(userQuery.Error.Length > 0)
-            return userQuery;
-        
-
+        // if(userQuery.Error.Length > 0)
+        //     return userQuery;
         SqlSolutionDTO solutionQuery = soluceQuery.ExecuteQuery();
         if (userQuery.Data is not null && solutionQuery.Data is not null)
             userQuery.CheckQueries(solutionQuery);
         else
             userQuery.Error = new string[] { "Errors while sending the data" };
 
+        if(sqldto.NewAnswer){
+            userQuery.TimeStamp = DateTimeOffset.Now;
 
-        var answer = new Answer 
-        {
-            Sql = sqldto.Query,
-            TimeStamp = DateTimeOffset.Now,
-            IsCorrect = userQuery.isCorrect,
-            AttemptId = sqldto.AttemptId,
-            QuestionId = sqldto.QuestionId                
-        };
+            var answer = new Answer 
+            {
+                Sql = sqldto.Query,
+                TimeStamp = DateTimeOffset.Now,
+                IsCorrect = userQuery.isCorrect,
+                AttemptId = sqldto.AttemptId,
+                QuestionId = sqldto.QuestionId                
+            };
 
-        _context.Answers.Add(answer);
-        await _context.SaveChangesAsync();
+            _context.Answers.Add(answer);
+            await _context.SaveChangesAsync();
 
-        var answerDTO = new AnswerDTO
-        {
-            Id = answer.Id,
-            Sql = answer.Sql,
-            TimeStamp = answer.TimeStamp,
-            IsCorrect = answer.IsCorrect,
-            AttemptId = answer.AttemptId,
-            QuestionId = answer.QuestionId,
-        };
-        
+            var answerDTO = new AnswerDTO
+            {
+                Id = answer.Id,
+                Sql = answer.Sql,
+                TimeStamp = answer.TimeStamp,
+                IsCorrect = answer.IsCorrect,
+                AttemptId = answer.AttemptId,
+                QuestionId = answer.QuestionId,
+            };
+        }else{
+            var answer = await _context.Answers.Where(a => a.AttemptId == sqldto.AttemptId && a.QuestionId == sqldto.QuestionId).OrderByDescending(a => a.TimeStamp).FirstOrDefaultAsync();
+            userQuery.TimeStamp = answer.TimeStamp;
+            Console.WriteLine(answer + " <-----------");
+        }
         return userQuery;
-
     }
 
     [AllowAnonymous]
